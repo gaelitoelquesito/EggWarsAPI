af521d0fef95b84f3141e05e394f40bb8b4f8eea
IhToN <ihton@hotmail.com>
2015-03-11 08:39:55 +0100

    Y más mierda al chest

--- src/es/minetsii/MiningCrates/chests/Chest.java
+++ src/es/minetsii/MiningCrates/chests/Chest.java
@@ -8,15 +8,17 @@ import org.bukkit.inventory.ItemStack;
 public class Chest {
 	private String name;
 	private String type;
+	private int probability;
 	private int amount;
 	private List<ItemStack> items;
 	
-	public Chest(String name, String type){
-		// Nombre;tipo:argumentos
-		// Cofre1;cash:1500
-		// Cofre2;items:Item1#amount#enchant,lvl-Item2#amount#enchant,lvl
+	public Chest(String name, String type, int probability){
+		// Nombre;Probabilidad;tipo:argumentos
+		// Cofre1;15;cash:1500
+		// Cofre2;5;items:Item1#amount#enchant,lvl-Item2#amount#enchant,lvl
 		this.name = name;
 		this.type = type;
+		this.probability = probability;
 		this.amount = 0;
 		this.items = new ArrayList<ItemStack>();
 	}
