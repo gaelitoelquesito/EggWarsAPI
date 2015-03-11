ff372eff9803b5fea423eb3265b543ae123ecd71
IhToN <ihton@hotmail.com>
2015-03-11 08:36:01 +0100

    Clase Chest

--- src/es/minetsii/MiningCrates/chests/Chest.java
+++ src/es/minetsii/MiningCrates/chests/Chest.java
@@ -1,5 +1,40 @@
 package es.minetsii.MiningCrates.chests;
 
-public class Chest {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.bukkit.inventory.ItemStack;
+
+public class Chest {
+	private String name;
+	private String type;
+	private int amount;
+	private List<ItemStack> items;
+	
+	public Chest(String name, String type){
+		this.name = name;
+		this.type = type;
+		this.amount = 0;
+		this.items = new ArrayList<ItemStack>();
+	}
+	
+	public String getName(){
+		return name;
+	}
+	public String getType(){
+		return type;
+	}
+	public int getAmount(){
+		return amount;
+	}
+	public List<ItemStack> getItems(){
+		return new ArrayList<ItemStack>(items);
+	}
+	
+	public void setAmount(int amount){
+		this.amount = amount;
+	}
+	public void setItems(List<ItemStack> items){
+		this.items = items;
+	}
 }
