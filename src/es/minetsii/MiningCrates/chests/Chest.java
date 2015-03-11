c3d74f7ea3003ce5f801eb862275650966c87bea
IhToN <ihton@hotmail.com>
2015-03-11 20:45:47 +0100

    Clase Chest cambiada

--- src/es/minetsii/MiningCrates/chests/Chest.java
+++ src/es/minetsii/MiningCrates/chests/Chest.java
@@ -1,48 +1,32 @@
 package es.minetsii.MiningCrates.chests;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.bukkit.inventory.ItemStack;
 
 public class Chest {
 	private String name;
-	private String type;
 	private int probability;
-	private int amount;
-	private List<ItemStack> items;
+	private Boolean isConsole;
+	private String command;
 	
-	public Chest(String name, String type, int probability){
-		// Nombre;Probabilidad;tipo:argumentos
-		// Cofre1;15;cash:1500,15-1100,6
-		// Cofre2;5;items:Item1#amount#enchant,lvl-Item2#amount#enchant,lvl
+	public Chest(String name, int probability, Boolean isConsole, String command){
+		// Nombre;Probabilidad;esConsola;comando
+		// Cofre1;15;true;eco give %p% 5000
+		// Cofre2;5;false;fly
 		this.name = name;
-		this.type = type;
 		this.probability = probability;
-		this.amount = 0;
-		this.items = new ArrayList<ItemStack>();
+		this.isConsole = isConsole;
+		this.command = command;
 	}
 	
 	public String getName(){
 		return name;
 	}
-	public String getType(){
-		return type;
-	}
 	public int getProbability(){
 		return probability;
 	}
-	public int getAmount(){
-		return amount;
+	public Boolean getIsConsole(){
+		return isConsole;
 	}
-	public List<ItemStack> getItems(){
-		return new ArrayList<ItemStack>(items);
-	}
-	
-	public void setAmount(int amount){
-		this.amount = amount;
-	}
-	public void setItems(List<ItemStack> items){
-		this.items = items;
+	public String getCommand(){
+		return command;
 	}
 }
--- src/es/minetsii/MiningCrates/chests/RandomChest.java
+++ src/es/minetsii/MiningCrates/chests/RandomChest.java
@@ -1,11 +1,8 @@
 package es.minetsii.MiningCrates.chests;
 
-import java.util.HashSet;
-import java.util.List;
 import java.util.Random;
 
 import org.bukkit.event.Listener;
-import org.bukkit.inventory.ItemStack;
 
 import es.minetsii.MiningCrates.MiningCrates;
 
@@ -20,8 +17,6 @@ public class RandomChest implements Listener {
 	
 	public void Random(){
 		Random r = new Random();
-		List<ItemStack> i = ch.getItems();
-		int size = i.size();
 	}
 
 }
