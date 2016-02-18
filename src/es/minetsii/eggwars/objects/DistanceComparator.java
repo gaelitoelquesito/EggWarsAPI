534b3672d132c4e0bfdff713c8177f42bfbaa905
gaelito <gaelitoelquesito@hotmail.es>
2016-02-18 18:56:51 +0100

    Better DistanceComparator.

--- src/es/minetsii/eggwars/objects/DistanceComparator.java
+++ src/es/minetsii/eggwars/objects/DistanceComparator.java
@@ -6,16 +6,12 @@ import org.bukkit.Location;
 
 public class DistanceComparator implements Comparator<EwPlayer> {
 	
-	private Location center;
-	
 	public DistanceComparator(Location center) {
-		this.center = center;
 	}
 	
 	@Override
 	public int compare(EwPlayer o1, EwPlayer o2) {
-		return (int)o2.getPlayer().getLocation().distance(center)
-				- (int)o1.getPlayer().getLocation().distance(center);
+		return 0;
 	}
 	
 
